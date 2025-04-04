1ff90aae532578affb88c2f27bbe81dea7deda50
Pedesalo <claus_katzer@estudante.sc.senai.br>
2025-04-03 22:17:22 -0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>PHP</title>
+		<meta charset="UTF-8">
+
+		<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
+		
+	</head>
+	<body>
+
+  <div class="row">
+    <div class="form-5">
+		<label >Nome: </label>
+		<input type="text">
+    </div>
+    <div class="col-4">
+		<label >Email: </label>
+		<input type="email">
+    </div>
+	<div class="col-3">
+		<label >Data de Nascimento: </label>
+		<input type="date">
+    </div>
+  </div>
+    
+
+		<script src="dist/js/bootstrap.min.js"></script>
+	</body>
+	
+</html>
